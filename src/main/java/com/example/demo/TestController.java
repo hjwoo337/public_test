a9ef2b0f5f9196970518f4475053ccbf29ab1d0e
DESKTOP-5GPF3MO\q-sol <hj.woo@q-sol.co.kr>
2025-02-26 15:11:56 +0900

    feat: Controller 테스트3

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController  // ✅ Rest API 응답을 위한 컨트롤러
 public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        return "test";
+        return "test";  // "test"라는 문자열이 그대로 응답됨
     }
 }
